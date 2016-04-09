ed23d45a1e956f351e63e813c028f1a6c51d2344
Adam Piech <adampiech7@op.pl>
2016-04-10 00:25:27 +0200

    Dodano test odejmowania Money.

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -11,26 +11,35 @@ import org.junit.Test;
 public class MoneyTest {
 
 	@Test(expected = IllegalArgumentException.class)
-    public void additionExceptionTest(){
-    	Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("EUR"));
+	public void additionExceptionTest() {
+		Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("EUR"));
 		Money secondMoney = new Money(new BigDecimal(1000), Currency.getInstance("USD"));
 		firstMoney.add(secondMoney);
-    }
-	
+	}
+
 	@Test(expected = IllegalArgumentException.class)
-    public void subtractExceptionTest(){
-    	Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("EUR"));
+	public void subtractExceptionTest() {
+		Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("EUR"));
 		Money secondMoney = new Money(new BigDecimal(1000), Currency.getInstance("USD"));
 		firstMoney.subtract(secondMoney);
-    }
+	}
+
+	@Test
+	public void additionTest() {
+		Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("USD"));
+		Money secondMoney = new Money(new BigDecimal(1000), Currency.getInstance("USD"));
+		Money result = firstMoney.add(secondMoney);
+		assertThat(result, Matchers.is(new Money(new BigDecimal(2500), Currency.getInstance("USD"))));
+		assertThat(firstMoney.getCurrency().getCurrencyCode(), Matchers.is("USD"));
+	}
 	
-	 @Test
-	 public void additionTest(){
-	    	Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("USD"));
-			Money secondMoney = new Money(new BigDecimal(1000), Currency.getInstance("USD"));
-			Money result = firstMoney.add(secondMoney);
-			assertThat(result, Matchers.is(new Money(new BigDecimal(2500), Currency.getInstance("USD"))));
-	        assertThat(firstMoney.getCurrency().getCurrencyCode(), Matchers.is("USD"));
-	    }
+	@Test
+	public void subtractTest() {
+		Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("USD"));
+		Money secondMoney = new Money(new BigDecimal(1000), Currency.getInstance("USD"));
+		Money result = firstMoney.subtract(secondMoney);
+		assertThat(result, Matchers.is(new Money(new BigDecimal(500), Currency.getInstance("USD"))));
+		assertThat(firstMoney.getCurrency().getCurrencyCode(), Matchers.is("USD"));
+	}
 
 }
