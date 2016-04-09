e789d31a3ac939dba82d9c5a33846987159a8dad
Adam Piech <adampiech7@op.pl>
2016-04-10 00:09:52 +0200

    Dodano test wyjątku dla dodawania Money.

---
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,19 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test(expected = IllegalArgumentException.class)
+    public void additionExceptionTest(){
+    	Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("EUR"));
+		Money secondMoney = new Money(new BigDecimal(1000), Currency.getInstance("USD"));
+		Money testResult = firstMoney.add(secondMoney);
+    }
+
+}
