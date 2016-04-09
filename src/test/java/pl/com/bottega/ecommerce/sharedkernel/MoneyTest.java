50a9f1a9673669e86f44b63defc15482353c8c26
Adam Piech <adampiech7@op.pl>
2016-04-10 00:24:24 +0200

    Dodano test dodawania Money.

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.math.BigDecimal;
 import java.util.Currency;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class MoneyTest {
@@ -13,14 +14,23 @@ public class MoneyTest {
     public void additionExceptionTest(){
     	Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("EUR"));
 		Money secondMoney = new Money(new BigDecimal(1000), Currency.getInstance("USD"));
-		Money testResult = firstMoney.add(secondMoney);
+		firstMoney.add(secondMoney);
     }
 	
 	@Test(expected = IllegalArgumentException.class)
     public void subtractExceptionTest(){
     	Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("EUR"));
 		Money secondMoney = new Money(new BigDecimal(1000), Currency.getInstance("USD"));
-		Money testResult = firstMoney.subtract(secondMoney);
+		firstMoney.subtract(secondMoney);
     }
+	
+	 @Test
+	 public void additionTest(){
+	    	Money firstMoney = new Money(new BigDecimal(1500), Currency.getInstance("USD"));
+			Money secondMoney = new Money(new BigDecimal(1000), Currency.getInstance("USD"));
+			Money result = firstMoney.add(secondMoney);
+			assertThat(result, Matchers.is(new Money(new BigDecimal(2500), Currency.getInstance("USD"))));
+	        assertThat(firstMoney.getCurrency().getCurrencyCode(), Matchers.is("USD"));
+	    }
 
 }
